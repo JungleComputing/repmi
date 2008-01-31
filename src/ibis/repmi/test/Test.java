a8948f713d09d3ae6b97c44392c222e782fa551f
Ana-Maria Oprescu <amo@cs.vu.nl>
2008-01-31 14:57:33 +0000

    added Usage to Test

--- src/ibis/repmi/test/Test.java
+++ src/ibis/repmi/test/Test.java
@@ -10,21 +10,28 @@ public class Test {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         VoidTest test = null;
+        long nops = Long.parseLong(args[1]);
+        int cpuId = Integer.parseInt(args[2]);
+        int ncpus = Integer.parseInt(args[3]);
+        String writerCluster = args[4];
+        
         switch (Integer.parseInt(args[0])) {
         case ONE_WRITER:
-            test = new OneWriteToMany(Long.parseLong(args[1]), Integer
-                    .parseInt(args[2]), 0, Integer.parseInt(args[3]), args[4]);
+            test = new OneWriteToMany(nops, cpuId, 0, ncpus, writerCluster);
             break;
         case TWO_WRITERS_1_CLUSTER:
-            test = new TwoWritersToMany(Long.parseLong(args[1]), Integer
-                    .parseInt(args[2]), 0, Integer.parseInt(args[3]));
+            test = new TwoWritersToMany(nops, cpuId, 0, ncpus);
             break;
         case TWO_WRITERS_2_CLUSTERS:
-            test = new TwoWritersToManyOn2Clusters(Long.parseLong(args[1]),
-                    Integer.parseInt(args[2]), 0, Integer.parseInt(args[3]));
+            test = new TwoWritersToManyOn2Clusters(nops, cpuId, 0, ncpus);
             break;
         default:
             System.out.println("No test selected");
+            System.out.println("Usage: Test test_type params");
+            System.out.println("\t test_type = 0 => ONE_WRITER");
+            System.out.println("\t test_type = 1 => TWO_WRITERS_1_CLUSTER");
+            System.out.println("\t test_type = 2 => TWO_WRITERS_2_CLUSTERS");    
+            System.out.println("\t params = No_operations cpuId No_cpus [writer's cluster]");            
             System.exit(0);
         }
 

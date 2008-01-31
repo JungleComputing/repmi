350736a76795cd1374bf2afc004f2f3f3fcce466
Ana-Maria Oprescu <amo@cs.vu.nl>
2008-01-31 14:49:27 +0000

    Unified all types of tests

--- src/ibis/repmi/test/Test.java
+++ src/ibis/repmi/test/Test.java
@@ -1,11 +1,33 @@
 package ibis.repmi.test;
 
 public class Test {
+    static final int ONE_WRITER = 0;
+
+    static final int TWO_WRITERS_1_CLUSTER = 1;
+
+    static final int TWO_WRITERS_2_CLUSTERS = 2;
+
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        OneWriteToMany test = new OneWriteToMany(Long.parseLong(args[0]),
-                Integer.parseInt(args[1]), 0, Integer.parseInt(args[2]),
-                args[3]);
+        VoidTest test = null;
+        switch (Integer.parseInt(args[0])) {
+        case ONE_WRITER:
+            test = new OneWriteToMany(Long.parseLong(args[1]), Integer
+                    .parseInt(args[2]), 0, Integer.parseInt(args[3]), args[4]);
+            break;
+        case TWO_WRITERS_1_CLUSTER:
+            test = new TwoWritersToMany(Long.parseLong(args[1]), Integer
+                    .parseInt(args[2]), 0, Integer.parseInt(args[3]));
+            break;
+        case TWO_WRITERS_2_CLUSTERS:
+            test = new TwoWritersToManyOn2Clusters(Long.parseLong(args[1]),
+                    Integer.parseInt(args[2]), 0, Integer.parseInt(args[3]));
+            break;
+        default:
+            System.out.println("No test selected");
+            System.exit(0);
+        }
+
         test.run();
         System.out.println("Test succeeded!");
         System.exit(0); // let shutdown hook terminate ibis
